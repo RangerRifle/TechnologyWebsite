0afa299e1f6f3c9952ff6cdf29833c9a74214b14
LOCKN017 <lockn017@d.umn.edu>
2025-04-06 23:18:21 -0500

    Initial push of assets and components

---
+++ components/Download.vue
@@ -0,0 +1,23 @@
+<template>
+    <button @click="downloadFile">Download Spaider</button>
+</template>
+
+<script>
+export default {
+    name: 'DownloadHome',
+    methods: {
+        downloadFile() {
+            const link = document.createElement('a');
+            link.href = 'https://github.com/RangerRifle/Spaider/blob/master/spaider.exe';
+            link.download = 'Spaider.exe';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="welcome">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WelcomeHome',
+    props: {
+        msg: String
+    }
+}
+</script>
